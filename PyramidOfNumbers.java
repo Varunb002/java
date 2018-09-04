33326d14de38fe26c0cef89c099199374fd52f5f
Varunb002 <42265899+Varunb002@users.noreply.github.com>
2018-09-04 21:15:52 +0530

    Add files via upload

---
+++ PyramidOfNumbers.java
@@ -0,0 +1,23 @@
+public class PyramidOfStars
+{
+  public static void CreatePyramid (int m)
+  {
+    for(int i=1; i<=m; i++)
+    {
+      for(int j=5; j>=i; j--)
+      {
+        System.out.print(" ");
+      }
+      for (int k=1; k<=i; k++)
+      {
+        System.out.print(k + " ");
+      }
+    System.out.println();
+    }
+  }
+
+  public static void main(String[] args)
+  {
+      CreatePyramid(5);
+  }
+}
