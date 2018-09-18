0c89846921cd27e07b32c39fe5847b8056a3e876
Varunb002 <42265899+Varunb002@users.noreply.github.com>
2018-09-18 12:37:42 +0530

    Add files via upload

---
+++ OneSeries.java
@@ -0,0 +1,16 @@
+public class OneSeries
+{
+  public static void OneSeries(int num)
+  {
+    for (int i=0;i<=num;i = i*10)
+    {
+      i = i+1;
+      System.out.println(i);
+    }
+  }
+
+  public static void main(String[] args)
+  {
+    OneSeries(11111);
+  }
+}
